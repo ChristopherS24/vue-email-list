3b03541a81d8d38d997e1e597235abc8b6d07826
ChristopherS24 <chryskies@gmail.com>
2024-09-03 10:25:00 +0200

    una email inserita

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,17 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            message: 'Template HTML CSS Vue'
+            emails: []
         };
+    },
+    created() {
+
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                const email = res.data.response;
+                console.log('email:', email);
+                this.emails.push(email);
+            });
     }
 }).mount('#app');
